8edba67ef85345c9bf569b16f483f8737f1f3dc8
Kareem Abouelseoud <88090312+kareem8996@users.noreply.github.com>
2024-12-20 13:15:33 +0200

    Plotly

--- streamlit_elements/frontend/components/modules/charts/Plotly.tsx
+++ streamlit_elements/frontend/components/modules/charts/Plotly.tsx
@@ -1,10 +1,11 @@
 import dynamic from "next/dynamic";
+import type { ComponentType } from "react";
+import type { PlotParams } from "react-plotly.js";
 import ElementsLoading from "../../ElementsLoading";
 
-const elements: ElementsRecord = {
-  Plot: dynamic(() => import("react-plotly.js"), { loading: ElementsLoading, ssr: false }),
-};
+const Plot = dynamic(() => import("react-plotly.js") as any, {
+  loading: ElementsLoading,
+  ssr: false,
+});
 
-const loadPlotly: ElementsLoader = element => elements[element];
-
-export default loadPlotly;
+export default Plot;
