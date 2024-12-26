c2f9eef064f5b6822b009c561fff18721eda4abb
Kareem Abouelseoud <88090312+kareem8996@users.noreply.github.com>
2024-12-26 09:37:24 +0200

    bug fixes

--- streamlit_elements/__init__.py
+++ streamlit_elements/__init__.py
@@ -1,7 +1,8 @@
+__version__ = "0.1.0"
 from streamlit_elements.core.frame import new_frame as _new_frame
 from streamlit_elements.core.exceptions import *
 from streamlit_elements.modules import *
-from streamlit_elements.version import __version__
+# from streamlit_elements.version import __version__
 
 
 def elements(key: str) -> None:
